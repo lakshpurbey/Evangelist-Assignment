bafadbf3214cb71b737bd7b589d411ffb8f51b31
sam purbey <you@example.com>
2022-06-27 16:40:58 +0545

    Project Setup

---
+++ Evangelist App/Evangelist_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Evangelist_AppApp.swift
+//  Evangelist App
+//
+//  Created by Laksh Purbey on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Evangelist_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
